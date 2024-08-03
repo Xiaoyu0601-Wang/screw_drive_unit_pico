4577d965f9e855c230bfed6b178bbba756ed65ad
Xiaoyu0601-Wang <micheal.wang@hotmail.com>
2024-08-03 19:52:50 +0900

    feat: add some can protocols

--- screw_drive_unit_pico/lib/Protocol/Protocol.h
+++ screw_drive_unit_pico/lib/Protocol/Protocol.h
@@ -1,4 +1,41 @@
 #ifndef _PROTOCOL_H_
 #define _PROTOCOL_H_
 
+#include "DEV_Config.h"
+#include "MCP2515.h"
+
+// typedef struct
+// {
+// 	uint32_t StdId;  
+// 	uint32_t ExtId;  
+// 	uint8_t IDE;     
+// 	uint8_t RTR;     
+// 	uint8_t DLC;     
+// 	uint8_t Data[8]; 
+// } CanTxMsg;
+
+// typedef struct
+// {
+// 	uint32_t StdId;  
+// 	uint32_t ExtId;  
+// 	uint8_t IDE;     
+// 	uint8_t RTR;     
+// 	uint8_t DLC;     
+// 	uint8_t Data[8]; 
+// 	uint8_t FMI;     
+// } CanRxMsg;
+
+typedef struct _UnitStatus
+{
+  uint16_t unitID; // Unit CAN ID
+  uint8_t  CanRxMsg[8];
+  uint8_t  CanTxMsg[8];
+  // CanTxMsg elmoCAN;  //  elmo报文结构体,用于向elmo写入数据
+  // tMode    Mode;     //
+} UnitStatus;
+
+UnitStatus unitStatus;
+
+bool protocol_update(struct repeating_timer *t);
+
 #endif
