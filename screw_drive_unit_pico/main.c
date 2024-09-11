e4e2a1cd55226b55e3b07d024d79dd178f2ade6e
Xiaoyu0601-Wang <micheal.wang@hotmail.com>
2024-09-11 23:33:12 +0900

    update: add some comments and enable timers

--- screw_drive_unit_pico/main.c
+++ screw_drive_unit_pico/main.c
@@ -12,17 +12,18 @@ bool led_timer_callback(struct repeating_timer *t)
         DEV_WIFI_LED_Write(unitStatus.ledStatus);
     }
 
-    // DEV_UART_WriteByte('A');
+    // DEV_UART_WriteByte('A');//for test
 
     return true;
 }
 
 bool can_timer_callback(struct repeating_timer *t)
 {
-    // MCP2515_Send(unitStatus.unitID, unitStatus.flashData, 8);
-
-    MCP2515_Receive(unitStatus.unitID, unitStatus.CanRxMsg);
-    Protocol_Update();
+    // MCP2515_Send(unitStatus.unitID, unitStatus.flashData, 8);//for test
+    if (MCP2515_Receive(unitStatus.unitID, unitStatus.CanRxMsg))
+    {
+        Protocol_Update();
+    }
 
     return true;
 }
@@ -34,18 +35,20 @@ bool ctrl_timer_callback(struct repeating_timer *t)
 
 int main(void)
 {
-    DEV_Delay_ms(200);
+    DEV_Delay_ms(100);
     DEV_Module_Init(uart_rx_irq);
-    Protocol_Init();
+    DEV_Delay_ms(10);
     MCP2515_Init();
-    DEV_Delay_ms(200);
+    Protocol_Init();
+    DEV_Delay_ms(10);
 
+    // use 499 and 9 for avoiding triggering interupt at the same time
     struct repeating_timer led_timer;
-    add_repeating_timer_ms(-500, led_timer_callback, NULL, &led_timer);
+    add_repeating_timer_ms(-499, led_timer_callback, NULL, &led_timer);
     struct repeating_timer can_timer;
-    add_repeating_timer_ms(-1000, can_timer_callback, NULL, &can_timer);
-    // struct repeating_timer ctrl_timer;
-    // add_repeating_timer_ms(-50, ctrl_timer_callback, NULL, &ctrl_timer);
+    add_repeating_timer_ms(-9, can_timer_callback, NULL, &can_timer);
+    struct repeating_timer ctrl_timer;
+    add_repeating_timer_ms(-20, ctrl_timer_callback, NULL, &ctrl_timer);
 
     while (1)
         tight_loop_contents();
